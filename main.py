accab3c5dbcf814343ed0c8c125512b546277c00
tinhou <tinhou.lan.cs@gmail.com>
2025-04-17 22:56:44 +0800

    删除单元测试代码

--- main.py
+++ main.py
@@ -4,14 +4,13 @@ from pydantic import ValidationError
 
 from config.LifeSpanConfig import lifespan
 from constant.BaseResponse import BaseResponse
-from controller import UserController
 from exception.GlobalExceptionHandler import handle_exception, validation_exception_handler, value_error_handler
-from routes import StockRoute
+from routes import StockDataRoute, MacroDataRoute
 
 # 主启动类添加路由
 app = FastAPI(lifespan=lifespan,)
-app.include_router(UserController.router)
-app.include_router(StockRoute.router)
+app.include_router(MacroDataRoute.router)
+app.include_router(StockDataRoute.router)
 
 # 注册全局异常处理器
 app.exception_handler(ValueError)(value_error_handler)
