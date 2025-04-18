85e8f8e1c3df3d8e1c75f7a50b9e3f4dbbc0bb1d
tinhou <tinhou.lan.cs@gmail.com>
2025-04-18 16:23:27 +0800

    添加docker部署配置

--- model/entity/BaseMeta/BaseMeta.py
+++ model/entity/BaseMeta/BaseMeta.py
@@ -1,12 +1,27 @@
-from typing import ClassVar
+import logging
 
 import databases
 import sqlalchemy
 import ormar
+import json
+
+from config.LoguruConfig import project_root, log
+from exception.BusinessException import BusinessException
 
 
-# 配置数据库连接
-DATABASE_URL = "mysql+aiomysql://root:131645@server:3306/macro_data_China"
+def get_url() -> str:
+    """获取数据库连接地址"""
+    try:
+        path = project_root / "resources" / "properties.json"
+        with open(path, "r", encoding="utf-8") as file:
+            data = json.load(file)
+            return data.get("MysqlUrl")
+    except FileNotFoundError as e:
+        log.exception(e)
+        raise BusinessException(code=500, msg="数据库配置文件不存在，请检查")
+
+
+DATABASE_URL = get_url()
 
 # 创建数据库连接和元数据对象
 database: databases.Database = databases.Database(DATABASE_URL)
