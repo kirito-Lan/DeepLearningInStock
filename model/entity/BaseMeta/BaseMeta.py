3c65c395a512f19ca13645c96ad25e7dfa224b51
tinhou <tinhou.lan.cs@gmail.com>
2025-04-19 10:48:55 +0800

    优化获取SQL连接方式

--- model/entity/BaseMeta/BaseMeta.py
+++ model/entity/BaseMeta/BaseMeta.py
@@ -1,4 +1,5 @@
 import logging
+import os
 
 import databases
 import sqlalchemy
@@ -10,15 +11,23 @@ from exception.BusinessException import BusinessException
 
 
 def get_url() -> str:
-    """获取数据库连接地址"""
-    try:
-        path = project_root / "resources" / "properties.json"
-        with open(path, "r", encoding="utf-8") as file:
-            data = json.load(file)
-            return data.get("MysqlUrl")
-    except FileNotFoundError as e:
-        log.exception(e)
-        raise BusinessException(code=500, msg="数据库配置文件不存在，请检查")
+    """获取数据库连接地址
+    """
+    host = os.getenv("DB_HOST", None)
+    port = os.getenv("DB_PORT", None)
+    user = os.getenv("DB_USER", None)
+    password = os.getenv("DB_PASSWORD", None)
+    database_name = os.getenv("DB_NAME", None)
+    if host is None or port is None or user is None or password is None or database_name is None:
+        try:
+            path = project_root / "resources" / "properties.json"
+            with open(path, "r", encoding="utf-8") as file:
+                data = json.load(file)
+                return data.get("MysqlUrl")
+        except FileNotFoundError as e:
+            log.exception(e)
+            raise BusinessException(code=500, msg="数据库配置不存在，请检查")
+    return f"mysql+aiomysql://{user}:{password}@{host}:{port}/{database_name}"
 
 
 DATABASE_URL = get_url()
@@ -32,6 +41,3 @@ ormar_base_config = ormar.OrmarConfig(
     database=database,
     metadata=metadata
 )
-
-
-
