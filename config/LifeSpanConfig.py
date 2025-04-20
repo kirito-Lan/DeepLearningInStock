1853beb7402a5e34d0861871354e706faddf22cc
tinhou <tinhou.lan.cs@gmail.com>
2025-04-20 12:25:21 +0800

    添加定时任务

--- config/LifeSpanConfig.py
+++ config/LifeSpanConfig.py
@@ -4,12 +4,14 @@ from fastapi import FastAPI
 
 from config.LoguruConfig import log
 from model.entity.BaseMeta.BaseMeta import database
+from scheduler.CleaningSchedule import scheduler
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     log.info("lifespan wakeUp")
     await database.connect()
+    scheduler.start()
     yield
     log.info("lifespan shutDown")
     await database.disconnect()
---
+++ scheduler/CleaningSchedule.py
@@ -0,0 +1,52 @@
+import os
+
+from apscheduler.schedulers.background import BackgroundScheduler
+
+from config.LoguruConfig import log, project_root
+from model.entity.MacroData import MacroData
+from model.entity.StockData import StockData
+
+# 定时任务
+scheduler = BackgroundScheduler()
+
+
+def clean_archive_file():
+    """
+    异步函数：清理生成的 csv 和 excel 文件。
+    Description:
+        清理生成的 csv 和 excel 文件，包括位于 project_root/resources/csvFiles 目录下的所有 csv 文件和
+        位于 project_root/resources/excelFiles 目录下的所有 excel 文件。
+
+        1. 使用 os.listdir() 获取指定目录下的所有文件名。
+        2. 检查文件名是否以 .csv 或 .xlsx 结尾。
+        3. 使用 os.remove() 删除符合条件的文件。
+
+    """
+    log.info("开始清理生成的 csv excel 文件")
+    csv_file_path = project_root / "resources" / "csvFiles"
+    excel_file_path = project_root / "resources" / "excelFiles"
+    for file in os.listdir(csv_file_path):
+        if file.endswith(".csv"):
+            os.remove(os.path.join(csv_file_path, file))
+    for file in os.listdir(excel_file_path):
+        if file.endswith(".xlsx"):
+            os.remove(os.path.join(excel_file_path, file))
+
+
+async def clean_db_bad_data():
+    """
+    清理 Stock Macro_data 库中的异常数据
+    偶发的 indicator_id 插入后异常为1的数据
+    :return: None
+    """
+    log.info("开始清理 Stock和Macro_data 库中的异常数据")
+    try:
+        await StockData.objects.filter(indicator_id=1).delete()
+        await MacroData.filter(indicator_id=1).delete()
+    except Exception as e:
+        log.exception(f"清理 Stock和Macro_data 库中的异常数据失败: {e}")
+
+# 每天凌晨 0 点执行一次清理任务
+scheduler.add_job(clean_archive_file, trigger='cron', hour=0)
+# 每天凌晨 1 点执行一次清理任务
+scheduler.add_job(clean_db_bad_data, trigger='cron', hour=1)
